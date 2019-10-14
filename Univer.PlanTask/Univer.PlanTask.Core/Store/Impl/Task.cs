3097c642c486b9ec65b4dda552bee6bbf45945e4
KerantiL <cz_159@mail.ru>
2019-10-15 10:30:07 +1300

    Update

---
+++ Univer.PlanTask/Univer.PlanTask.Core/Store/Impl/Task.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Univer.PlanTask.Core.Store;
+
+namespace Univer.PlanTask.ConsoleApp
+{
+    public class Task : IEntity
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Task()
+        {
+            Id = 0;
+            Name = "";
+        }
+
+        public Task(long _id)
+        {
+            Id = _id;
+            Name = "";
+        }
+    }
+}
